f05388c4e42ddb3719b203f3c3ae10833a054062
Vishnu Kumar R <132224762+vishnufasv@users.noreply.github.com>
2025-01-07 23:18:32 +0530

    Size
    Primitive Data Type Size

---
+++ size.java
@@ -0,0 +1,19 @@
+import java.lang.*;
+
+class size
+{
+public static void main(String args[])
+	{
+	System.out.println("-------------------------------------------------- ");
+	System.out.println(" There Are 8 Type Primitive Data Types and Their Size ");
+System.out.println("-------------------------------------------------- ");
+	System.out.println("1.) The Size of Byte is :- "+ Byte.SIZE +" Bits");
+	System.out.println("2.) The Size of Short is :- "+ Short.SIZE +" Bits");
+	System.out.println("3.) The Size of Int is :- "+ Integer.SIZE +" Bits");
+	System.out.println("4.) The Size of Long is :- "+  Long.SIZE +" Bits");
+	System.out.println("5.) The Size of Float is :- "+ Float.SIZE +" Bits");
+	System.out.println("6.) The Size of Char is :- "+ Character.SIZE +" Bits");
+	System.out.println("7.) The Size of Double is :- "+ Double.SIZE +" Bits");
+	System.out.println("*.) The Size of Boolean is :- True or False");
+	}
+}
