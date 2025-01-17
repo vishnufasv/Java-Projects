fffe850d98d247b856172b00ebd9f42c2b1ca829
Vishnu Kumar R <132224762+vishnufasv@users.noreply.github.com>
2025-01-17 11:59:18 +0530

    Adding 2 Numbers

---
+++ AddingTwoNo.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class AddTwoNumbers
+{
+public static void main(String[] args)
+{
+Scanner ab=new Scanner(System.in);
+
+int num1=ab.nextInt();
+int num2=ab.nextInt();
+
+if((num1>=500 && num1<=520)&&(num2>=500 && num2<=520))
+{
+
+int sum=num1+num2;
+System.out.println("add two numbers "+num1+ " and " +num2+ " result is " +sum);
+}
+else
+{
+System.out.println("both condition must be in the range of 500 to520");
+}
+}
+}
